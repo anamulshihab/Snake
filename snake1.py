e79d59b65a13479290fe402a3cc46e0256b15b64
shihab007 <ashihab@itcollge.ee>
2017-03-16 21:09:23 +0200

    ::

--- snake1.py
+++ snake1.py
@@ -1,27 +1,169 @@
-
-
-''' #1 import some stuff '''
 import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
 from random import randint
 
-''' #2 Define some variables '''
-WIDTH = 35 # total game cell width and height
+WIDTH = 35
 HEIGHT = 20
-MAX_X = WIDTH - 2 # sets max inside the game, so the snake wont hit the border
+MAX_X = WIDTH - 2
 MAX_Y = HEIGHT - 2
-SNAKE_LENGTH = 5 # Starting snake length
+SNAKE_LENGTH = 5
 SNAKE_X = SNAKE_LENGTH + 1
 SNAKE_Y = 3
-TIMEOUT = 100 #this controls the speed of the game
+TIMEOUT = 100
 
-''' #3 make Snake Oject '''
 class Snake(object):
-    def __init__(self):
-        self.x = 'working...'
+    REV_DIR_MAP = {
+        KEY_UP: KEY_DOWN, KEY_DOWN: KEY_UP,
+        KEY_LEFT: KEY_RIGHT, KEY_RIGHT: KEY_LEFT,
+    }
 
-    def method_a(self, foo):
-        print self.x + ' ' + foo
+    def __init__(self, x, y, window):
+        self.body_list = []
+        self.hit_score = 0
+        self.timeout = TIMEOUT
 
-snake = Snake()               # We do not pass any argument to the __init__ method
-snake.method_a('snake is coming!!') # We only pass a single argument
+        for i in range(SNAKE_LENGTH, 0, -1):
+            self.body_list.append(Body(x - i, y))
+
+        self.body_list.append(Body(x, y, '0'))
+        self.window = window
+        self.direction = KEY_RIGHT
+        self.last_head_coor = (x, y)
+        self.direction_map = {
+            KEY_UP: self.move_up,
+            KEY_DOWN: self.move_down,
+            KEY_LEFT: self.move_left,
+            KEY_RIGHT: self.move_right
+        }
+
+    @property
+    def score(self):
+        return 'Score : {}'.format(self.hit_score)
+
+
+    
+    def add_body(self, body_list):
+        self.body_list.extend(body_list)
+
+    # Eat food fucntion, functionality for snake eating food
+    def eat_food(self, food):
+        
+        food.reset()
+        
+        body = Body(self.last_head_coor[0], self.last_head_coor[1])
+        
+        self.body_list.insert(-1, body)
+        # update game score
+        self.hit_score += 1
+        
+        if self.hit_score % 3 == 0:
+            self.timeout -= 5
+            self.window.timeout(self.timeout)
+
+    
+    @property
+    def collided(self): #  if snake head hits body part end game
+        return any([body.coor == self.head.coor
+                    for body in self.body_list[:-1]])
+
+    # Updating function for animation
+    def update(self):
+       
+        last_body = self.body_list.pop(0)
+        last_body.x = self.body_list[-1].x
+        last_body.y = self.body_list[-1].y
+        self.body_list.insert(-1, last_body)
+        self.last_head_coor = (self.head.x, self.head.y)
+        self.direction_map[self.direction]()
+
+    # change direction function
+    def change_direction(self, direction):
+            if direction != Snake.REV_DIR_MAP[self.direction]:
+            self.direction = direction
+    
+    def render(self):
+        for body in self.body_list:
+            
+            self.window.addstr(body.y, body.x, body.char)
+
+       @property
+    #  the snake head
+    def head(self):
+        
+        return self.body_list[-1]
+
+    # snake head initial coordinate
+    @property
+    def coor(self):
+        return self.head.x, self.head.y
+
+    # move up function
+    def move_up(self):
+        self.head.y -= 1
+        if self.head.y < 1:
+            self.head.y = MAX_Y
+    # move down
+    def move_down(self):
+        self.head.y += 1
+        if self.head.y > MAX_Y:
+            self.head.y = 1
+    # move left
+    def move_left(self):
+        self.head.x -= 1
+        if self.head.x < 1:
+            self.head.x = MAX_X
+    # move rigth
+    def move_right(self):
+        self.head.x += 1
+        if self.head.x > MAX_X:
+            self.head.x = 1
+
+class Body(object):
+    def __init__(self, x, y, char='='):
+        self.x = x
+        self.y = y
+        self.char = char
+
+    @property
+    def coor(self):
+        return self.x, self.y
+
+class Food(object):
+    def __init__(self, window, char='&'):
+        self.x = randint(1, MAX_X)
+        self.y = randint(1, MAX_Y)
+        self.char = char
+        self.window = window
+
+    def render(self):
+        self.window.addstr(self.y, self.x, self.char)
+
+    def reset(self):
+        self.x = randint(1, MAX_X)
+        self.y = randint(1, MAX_Y)
+
+
+if __name__ == '__main__':
+    curses.initscr()
+    curses.beep()
+    curses.beep()
+    window = curses.newwin(HEIGHT, WIDTH, 0, 0)
+    window.timeout(TIMEOUT)
+    window.keypad(1)
+    curses.noecho()
+    curses.curs_set(0)
+    window.border(0)
+
+    snake = Snake(SNAKE_X, SNAKE_Y, window)
+    food = Food(window, '*')
+
+    while True:
+        window.clear()
+        window.border(0)
+        snake.render()
+        food.render()
+        window.addstr(0, 5, snake.score)
+        event = window.getch()
+
+
+    curses.endwin()
